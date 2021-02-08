8b793b42f350aba48ece0816f49b9f839fb83163
Arthur Dvorak <artdvorak@yandex.ru>
2021-02-08 20:40:45 +0300

    commit

---
+++ AutoShop/Data/Mocks/MockCategory.cs
@@ -0,0 +1,24 @@
+﻿using AutoShop.Data.Interfaces;
+using AutoShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoShop.Data.Mocks
+{
+    public class MockCategory : ICarsCategory
+    {
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category {categoryName = "Электромобили", desc = "Экологичный вид транспорта" },
+                    new Category {categoryName = "Классические автомобили", desc ="Машины с ДВС" }
+                };
+            }
+        }
+    }
+}
